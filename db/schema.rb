e2dd095a68050526f898d207b23956233d570195
jdgsb <uber.tubers@gmail.com>
2015-05-20 18:15:50 -0700

    still master having errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513041641) do
+ActiveRecord::Schema.define(version: 20150521004744) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -29,12 +29,15 @@ ActiveRecord::Schema.define(version: 20150513041641) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "post_id"
   end
 
+  add_index "posts", ["post_id"], name: "index_posts_on_post_id"
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "summaries", force: :cascade do |t|
+    t.text     "abstract"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
