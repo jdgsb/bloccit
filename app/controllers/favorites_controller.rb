7af222bead68eaa94d120a1500f691a7d16e595c
jdgsb <uber.tubers@gmail.com>
2015-06-13 18:39:25 -0700

    error in nil class

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,33 @@
+class FavoritesController < ApplicationController
+
+  def create
+     post = Post.find(params[:post_id])
+     favorite = current_user.favorites.build(post: post)
+ 
+     authorize favorite
+
+     if favorite.save
+      flash[:notice] = "Favorite saved."
+      redirect_to [@post.topic, @post]
+     else
+      flash[:error] = "There was an error saving the favorite. Please try again."
+      render :new
+     end
+  end
+
+  def destroy
+     post = Post.find(params[:post_id])
+     favorite = current_user.favorites.build(post: post)
+     
+     authorize favorite
+
+     if favorite.destroy
+       flash[:notice] = "Favorite was deleted successfully."
+       redirect_to [@post.topic, @post]
+     else
+       flash[:notice] = "There was an error deleting the favorite."
+       redirect_to [@post.topic, @post]
+     end
+  end
+
+end
---
+++ app/policies/favorite_policy.rb
@@ -0,0 +1,9 @@
+class FavoritePolicy < ApplicationPolicy
+
+  def create?
+     user.present?
+  end
+
+
+
+end
