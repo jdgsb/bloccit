a6fa293852e0f3f0e5ee19268432bbe409e17a45
jdgsb <uber.tubers@gmail.com>
2015-06-21 19:18:24 -0700

    completed public profiles assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,4 +1,7 @@
  require 'rails_helper'
+
+ include Warden::Test::Helpers
+ Warden.test_mode!
  
  describe "Visiting profiles" do
  
@@ -24,4 +27,22 @@
      end
  
    end
+
+   describe "signed in" do
+
+    before do
+      login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@post.title)
+    end
+
+
  end
+end
